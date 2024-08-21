b04084bbabc6b3046a679e283556de5ea5b60c0e
zakrad <daniyal.rad@gmail.com>
2024-08-21 10:41:19 +0330

    feat(interfaces): add upgrade module interface

---
+++ test/test.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.20;
+
+contract TestContract {
+    // solhint-disable-next-line no-empty-blocks
+    function testEmptyContract() public {
+        // Empty test to pass ci
+    }
+}
