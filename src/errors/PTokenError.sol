9b31051ca723e4a0398407382897922df64f45a3
zakrad <daniyal.rad@gmail.com>
2024-08-21 13:50:19 +0330

    feat(error): add utils and common error

---
+++ src/errors/PTokenError.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * @title Library for pToken errors.
+ */
+library PTokenError {
+    error SetReserveFactorFreshCheck();
+
+    error SetReserveFactorBoundsCheck();
+}
