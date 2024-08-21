d1a7135ca311d4618db11359fa14a26751a44d58
zakrad <daniyal.rad@gmail.com>
2024-08-22 00:19:38 +0330

    refactor(interface): modify irm interface

--- src/interfaces/IInterestRateModel.sol
+++ src/interfaces/IInterestRateModel.sol
@@ -2,20 +2,45 @@
 pragma solidity 0.8.20;
 
 interface IInterestRateModel {
+    event NewInterestParams(
+        uint256 baseRatePerSecond,
+        uint256 multiplierPerSecond,
+        uint256 jumpMultiplierPerSecond,
+        uint256 kink
+    );
+
     /**
      * @notice Calculates the utilization rate of the market: `borrows / (cash + borrows - reserves)`
      */
-    function getUtilization() external view returns (uint256);
+    function getUtilization(uint256 cash, uint256 borrows, uint256 reserves)
+        external
+        view
+        returns (uint256);
 
     /**
-     * @notice Returns the current per-second borrow interest rate for this pToken
+     * @notice Calculates the current borrow rate per second
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market
      * @return The borrow interest rate per second, scaled by 1e18
      */
-    function borrowRatePerSecond() external view returns (uint256);
+    function getBorrowRate(uint256 cash, uint256 borrows, uint256 reserves)
+        external
+        view
+        returns (uint256);
 
     /**
-     * @notice Returns the current per-second borrow interest rate for this pToken
+     * @notice Calculates the current supply rate per second
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market
+     * @param reserveFactorMantissa The current reserve factor for the market
      * @return The supply interest rate per second, scaled by 1e18
      */
-    function supplyRatePerSecond() external view returns (uint256);
+    function getSupplyRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves,
+        uint256 reserveFactorMantissa
+    ) external view returns (uint256);
 }
