e402e2005fa09793b20fd98c476d3cf3c132c318
zakrad <daniyal.rad@gmail.com>
2024-08-29 10:13:34 +0330

    feat(re): add redeem and transfer hooks

--- src/modules/riskEnigne/RiskEngineModule.sol
+++ src/modules/riskEnigne/RiskEngineModule.sol
@@ -13,6 +13,124 @@ import {OwnableMixin} from "@utils/OwnableMixin.sol";
 contract RiskEngineModule is IRiskEngine, RiskEngineStorage, OwnableMixin {
     using ExponentialNoError for ExponentialNoError.Exp;
     using ExponentialNoError for uint256;
+    /**
+     * @inheritdoc IRiskEngine
+     */
+    function redeemAllowed(address pToken, address redeemer, uint256 redeemTokens)
+        external
+        view
+        returns (uint256)
+    {
+        uint256 allowed = redeemAllowedInternal(pToken, redeemer, redeemTokens);
+        if (allowed != uint256(RiskEngineError.Error.NO_ERROR)) {
+            return allowed;
+        }
+
+        return uint256(RiskEngineError.Error.NO_ERROR);
+    }
+
+    /**
+     * @inheritdoc IRiskEngine
+     */
+    function transferAllowed(address pToken, address src, uint256 transferTokens)
+        external
+        view
+        returns (uint256)
+    {
+        // Pausing is a very serious situation - we revert to sound the alarms
+        if (_getRiskEngineStorage().transferGuardianPaused) {
+            revert RiskEngineError.TransferPaused();
+        }
+
+        // Currently the only consideration is whether or not
+        //  the src is allowed to redeem this many tokens
+        uint256 allowed = redeemAllowedInternal(pToken, src, transferTokens);
+        if (allowed != uint256(RiskEngineError.Error.NO_ERROR)) {
+            return allowed;
+        }
+
+        return uint256(RiskEngineError.Error.NO_ERROR);
+    }
+
+    /**
+     * @notice Add the market to the borrower's "assets in" for liquidity calculations
+     * @param pToken The market to enter
+     * @param borrower The address of the account to modify
+     * @return Success indicator for whether the market was entered
+     */
+    function addToMarketInternal(IPToken pToken, address borrower)
+        internal
+        returns (RiskEngineError.Error)
+    {
+        Market storage marketToJoin = _getRiskEngineStorage().markets[address(pToken)];
+
+        if (!marketToJoin.isListed) {
+            // market is not listed, cannot join
+            return RiskEngineError.Error.MARKET_NOT_LISTED;
+        }
+
+        if (marketToJoin.accountMembership[borrower] == true) {
+            // already joined
+            return RiskEngineError.Error.NO_ERROR;
+        }
+
+        // survived the gauntlet, add to list
+        // NOTE: we store these somewhat redundantly as a significant optimization
+        //  this avoids having to iterate through the list for the most common use cases
+        //  that is, only when we need to perform liquidity checks
+        //  and not whenever we want to check if an account is in a particular market
+        marketToJoin.accountMembership[borrower] = true;
+        _getRiskEngineStorage().accountAssets[borrower].push(pToken);
+
+        emit MarketEntered(pToken, borrower);
+
+        return RiskEngineError.Error.NO_ERROR;
+    }
+
+    /**
+     * @notice Determine the current account liquidity with respect to collateral requirements
+     * @return (possible error code,
+     *             account liquidity in excess of collateral requirements,
+     *          account shortfall below collateral requirements)
+     */
+    function getAccountLiquidityInternal(address account)
+        internal
+        view
+        returns (RiskEngineError.Error, uint256, uint256)
+    {
+        return getHypotheticalAccountLiquidityInternal(
+            account, IPToken(address(0)), 0, 0, _getLiquidationThreshold
+        );
+    }
+
+    function redeemAllowedInternal(address pToken, address redeemer, uint256 redeemTokens)
+        internal
+        view
+        returns (uint256)
+    {
+        if (!_getRiskEngineStorage().markets[pToken].isListed) {
+            return uint256(RiskEngineError.Error.MARKET_NOT_LISTED);
+        }
+
+        /* If the redeemer is not 'in' the market, then we can bypass the liquidity check */
+        if (!_getRiskEngineStorage().markets[pToken].accountMembership[redeemer]) {
+            return uint256(RiskEngineError.Error.NO_ERROR);
+        }
+
+        /* Otherwise, perform a hypothetical liquidity check to guard against shortfall */
+        (RiskEngineError.Error err,, uint256 shortfall) =
+        getHypotheticalAccountLiquidityInternal(
+            redeemer, IPToken(pToken), redeemTokens, 0, _getCollateralFactor
+        );
+        if (err != RiskEngineError.Error.NO_ERROR) {
+            return uint256(err);
+        }
+        if (shortfall > 0) {
+            return uint256(RiskEngineError.Error.INSUFFICIENT_LIQUIDITY);
+        }
+
+        return uint256(RiskEngineError.Error.NO_ERROR);
+    }
 
     /**
      * @notice Determine what the account liquidity would be if the given amounts were redeemed/borrowed
