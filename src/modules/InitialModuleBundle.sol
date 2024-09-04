4251202a8adc581352488704a7c55b8abd741c94
zakrad <daniyal.rad@gmail.com>
2024-09-04 19:23:03 +0330

    feat(module): add initial module bundle for the proxy

---
+++ src/modules/InitialModuleBundle.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Initializable} from
+    "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@modules/common/OwnableModule.sol";
+import "@modules/common/UpgradeModule.sol";
+
+// solhint-disable-next-line no-empty-blocks
+contract InitialModuleBundle is OwnableModule, UpgradeModule, Initializable {
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        _getOwnableStorage().owner = initialOwner;
+    }
+}
