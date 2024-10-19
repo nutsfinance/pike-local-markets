730693924323090d29eda1d5f7d6ca441862784d
zakrad <daniyal.rad@gmail.com>
2024-10-20 00:10:56 +0330

    chore: bump-pragma-0.8.28

--- src/local-market/modules/common/RBACModule.sol
+++ src/local-market/modules/common/RBACModule.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity 0.8.28;
 
 import {IRBAC} from "@interfaces/IRBAC.sol";
 import {RBACMixin} from "@utils/RBACMixin.sol";
--- test/mocks/Diamond/Diamond.sol
+++ test/mocks/Diamond/Diamond.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.25;
+pragma solidity >=0.8.28;
 
 import {LibDiamond} from "./libraries/LibDiamond.sol";
 import {IDiamondCut} from "./IDiamondCut.sol";
