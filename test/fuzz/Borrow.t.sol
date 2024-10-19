412707d659590534bbe455048f25f1b69e0501ec
zakrad <daniyal.rad@gmail.com>
2024-10-20 00:10:56 +0330

    chore: bump-pragma-0.8.28

--- test/fuzz/Borrow.t.sol
+++ test/fuzz/Borrow.t.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.25;
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "forge-std/Test.sol";
--- test/helpers/TestDeploy.sol
+++ test/helpers/TestDeploy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity 0.8.28;
 
 import {TestSetters} from "@helpers/TestSetters.sol";
 import {DiamondCutFacet} from "@mocks/Diamond/facets/DiamondCutFacet.sol";
