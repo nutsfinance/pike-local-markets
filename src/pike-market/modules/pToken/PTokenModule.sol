37472e5f968af3049c27b1fc9bd8acca8ac0fbbc
zakrad <daniyal.rad@gmail.com>
2025-08-15 00:09:39 +0700

    fix: prevent DoS in liquidation by minimal repayment (#39)

--- src/pike-market/modules/pToken/PTokenModule.sol
+++ src/pike-market/modules/pToken/PTokenModule.sol
@@ -1142,11 +1142,11 @@ contract PTokenModule is IPToken, PTokenStorage, OwnableMixin, RBACStorage {
         /* Fail if repayAmount = 0 */
         require(repayAmount != 0, PTokenError.LiquidateCloseAmountIsZero());
 
-        /* Fail if repayAmount = type(uint256).max */
-        require(
-            repayAmount != type(uint256).max, PTokenError.LiquidateCloseAmountIsUintMax()
-        );
-
+        /* Max Close if repayAmount = type(uint256).max */
+        if (repayAmount == type(uint256).max) {
+            repayAmount = _getPTokenStorage().riskEngine.closeFactor(address(this)).toExp(
+            ).mul_ScalarTruncate(borrowBalanceStoredInternal(borrower));
+        }
         /* Fail if repayBorrow fails */
         uint256 actualRepayAmount = repayBorrowFresh(liquidator, borrower, repayAmount);
 
